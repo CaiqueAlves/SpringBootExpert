e7b232aa379eb4ff492fbd45e9dea183310d4db2
CaiqueAlves <caiquealves858@gmail.com>
2023-03-28 00:17:38 -0300

    criando annotations costumizadas de configuracao

--- src/main/java/io/github/caique/MinhaConfiguration.java
+++ src/main/java/io/github/caique/MinhaConfiguration.java
@@ -1,8 +1,16 @@
 package io.github.caique;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 @Configuration
+@Development
 public class MinhaConfiguration {
-
+    @Bean
+    public CommandLineRunner executar(){
+        return args ->{
+            System.out.println("RODANDO A CONFIGURAÇÃO DE DESENVOLVIMENTO");
+        };
+    }
 }
