b981ed05cd6ef6b98ddd099579ea5608da2bbb90
CaiqueAlves <caiquealves858@gmail.com>
2023-03-28 23:03:59 -0300

    Criando as classes de modelo 'domain'

---
+++ src/main/java/io/github/caique/domain/entity/Pedido.java
@@ -0,0 +1,11 @@
+package io.github.caique.domain.entity;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+public class Pedido {
+    private Integer id;
+    private Cliente cliente;
+    private LocalDate dataPedido;
+    private BigDecimal total;
+}
