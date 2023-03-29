af050ee6055b695c4522408957b4fddcf59f7dac
CaiqueAlves <caiquealves858@gmail.com>
2023-03-28 22:53:04 -0300

    Configurando, obtendo conexao com o banco de dados e criacao de scripts do banco de dados

--- src/main/java/io/github/caique/Animal.java
+++
@@ -1,5 +0,0 @@
-package io.github.caique;
-
-public interface Animal {
-    void fazerBarulho();
-}
--- src/main/java/io/github/caique/repository/ClientesRepository.java
+++
@@ -1,9 +0,0 @@
-package io.github.caique.repository;
-
-import io.github.caique.model.Cliente;
-
-public class ClientesRepository {
-    public void persistir(Cliente cliente) {
-        //acessa a base e salva o cliente
-    }
-}
