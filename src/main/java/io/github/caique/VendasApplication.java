ffc479f9a5f09b94a4dfeec30674f283e64c5a02
CaiqueAlves <caiquealves858@gmail.com>
2023-03-30 00:05:02 -0300

    concluindo cadastro de cliente

--- src/main/java/io/github/caique/VendasApplication.java
+++ src/main/java/io/github/caique/VendasApplication.java
@@ -16,11 +16,39 @@ public class VendasApplication {
     @Bean
     public CommandLineRunner init(@Autowired Clientes clientes){
         return args -> {
+
+            System.out.println("SALVANDO CLIENTES");
             clientes.salvar(new Cliente("Caique"));
             clientes.salvar(new Cliente("Dougllas"));
 
             List<Cliente> todosClientes = clientes.obterTodos();
             todosClientes.forEach(System.out::println);
+
+            System.out.println("ATUALIZANDO CLIENTES");
+            todosClientes.forEach(c ->{
+                c.setNome(c.getNome() + " atualizado.");
+                clientes.atualizar(c);
+            });
+
+            todosClientes = clientes.obterTodos();
+            todosClientes.forEach(System.out::println);
+
+            System.out.println("--BUSCANDO CLIENTES--");
+            clientes.buscarPorNome("Cai").forEach(System.out::println);
+            System.out.println("--BUSCANDO CLIENTES--");
+
+
+            /*System.out.println("DELETANDO CLIENTES");
+            clientes.obterTodos().forEach(c->{
+                clientes.deletar(c);
+            });*/
+
+            todosClientes = clientes.obterTodos();
+            if(todosClientes.isEmpty()){
+                System.out.println("NENHUM CLIENTE ENCONTRADO");
+            }else {
+                todosClientes.forEach(System.out::println);
+            }
         };
     }
 
--- src/main/java/io/github/caique/domain/repsitorio/Clientes.java
+++ src/main/java/io/github/caique/domain/repsitorio/Clientes.java
@@ -16,22 +16,49 @@ public class Clientes {
     @Autowired
     private JdbcTemplate jdbcTemplate;
 
-    public static String INSERT="insert into cliente (nome) values (?)";
-    public static String SELECT_ALL="select * from cliente";
+    private static String INSERT="insert into cliente (nome) values (?)";
+    private static String SELECT_ALL="select * from cliente";
+    private static String UPDATE = "update cliente set nome = ? where id = ?";
+    private static String DELETE = "delete from cliente where id = ?";
 
     public Cliente salvar(Cliente cliente){
         jdbcTemplate.update(INSERT, new Object[]{cliente.getNome()});
         return cliente;
     }
 
+    public Cliente atualizar(Cliente cliente){
+        jdbcTemplate.update(UPDATE, new Object[]{
+                cliente.getNome(), cliente.getId()} );
+        return cliente;
+    }
+
+    public void deletar(Cliente cliente){
+        deletar(cliente.getId());
+    }
+
+    public List<Cliente> buscarPorNome(String nome){
+        return jdbcTemplate.query(
+                SELECT_ALL.concat(" where nome like ? "),
+                new Object[]{"%" + nome + "%"},
+                obterClienteMapper());
+    }
+
+    public void deletar(Integer id){
+        jdbcTemplate.update(DELETE, new Object[]{id});
+    }
+
     public List<Cliente> obterTodos(){
-        return jdbcTemplate.query(SELECT_ALL, new RowMapper<Cliente>() {
+        return jdbcTemplate.query(SELECT_ALL, obterClienteMapper());
+    }
+
+    private RowMapper<Cliente> obterClienteMapper() {
+        return new RowMapper<Cliente>() {
             @Override
             public Cliente mapRow(ResultSet resultSet, int i) throws SQLException {
                 Integer id = resultSet.getInt("id");
                 String nome = resultSet.getString("nome");
                 return new Cliente(id, nome);
             }
-        });
+        };
     }
 }
