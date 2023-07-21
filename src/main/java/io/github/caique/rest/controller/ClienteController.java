5a2eab6bb3fbafc72cd2869db5628090ae3e253c
CaiqueAlves <caiquealves858@gmail.com>
2023-07-20 23:16:06 -0300

    Desenvolvimento dos putMapping e getMapping de cliente

--- src/main/java/io/github/caique/rest/controller/ClienteController.java
+++ src/main/java/io/github/caique/rest/controller/ClienteController.java
@@ -2,10 +2,14 @@ package io.github.caique.rest.controller;
 
 import io.github.caique.domain.entity.Cliente;
 import io.github.caique.domain.repository.Clientes;
+import org.apache.coyote.Response;
+import org.springframework.data.domain.Example;
+import org.springframework.data.domain.ExampleMatcher;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.Optional;
 
 @Controller
@@ -42,7 +46,31 @@ public class ClienteController {
                 clientes.delete(cliente.get());
                 return ResponseEntity.noContent().build();
             }
-
             return ResponseEntity.notFound().build();
+    }
+
+    @PutMapping("/api/clientes/{id}")
+    @ResponseBody
+    public ResponseEntity update (@PathVariable Integer id, @RequestBody Cliente cliente){
+
+        return  clientes.
+                findById(id)
+                .map(clienteExistente -> {
+                    cliente.setId(clienteExistente.getId());
+                    clientes.save(cliente);
+                    return ResponseEntity.noContent().build();
+                }).orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+    @GetMapping("/api/clientes")
+    public ResponseEntity find( Cliente filtro){
+        ExampleMatcher matcher = ExampleMatcher.
+                                matching().
+                                withIgnoreCase().
+                                withStringMatcher(
+                                        ExampleMatcher.StringMatcher.CONTAINING);
+        Example example = Example.of(filtro, matcher);
+        List<Cliente> lista = clientes.findAll(example);
+        return ResponseEntity.ok(lista);
     }
 }
