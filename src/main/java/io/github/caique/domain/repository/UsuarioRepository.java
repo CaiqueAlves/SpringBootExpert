0c198d986126c2826fe42bac29e3c52aeeb0cf11
CaiqueAlves <caiquealves858@gmail.com>
2023-08-21 22:10:09 -0300

    desenvolvimento do Spring Security

---
+++ src/main/java/io/github/caique/domain/repository/UsuarioRepository.java
@@ -0,0 +1,10 @@
+package io.github.caique.domain.repository;
+
+import io.github.caique.domain.entity.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UsuarioRepository extends JpaRepository<Usuario, Integer> {
+    Optional<Usuario> findByLogin(String login);
+}
---
+++ src/main/java/io/github/caique/rest/controller/UsuarioController.java
@@ -0,0 +1,26 @@
+package io.github.caique.rest.controller;
+
+import io.github.caique.domain.entity.Usuario;
+import io.github.caique.service.impl.UsuarioServiceImpl;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.*;
+import javax.validation.Valid;
+
+@RestController
+@RequestMapping("/api/usuarios")
+@RequiredArgsConstructor
+public class UsuarioController {
+
+    private final UsuarioServiceImpl usuarioService;
+    private final PasswordEncoder passwordEncoder;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Usuario salvar(@RequestBody @Valid Usuario usuario){
+        String senhaCriptografada = passwordEncoder.encode(usuario.getSenha());
+        usuario.setSenha(senhaCriptografada);
+        return usuarioService.salvar(usuario);
+    }
+}
